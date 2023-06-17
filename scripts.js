5ba4ce2b4e5e0baf14996cee823f348e39523d93
Nima Abed <abodnima84@gmail.com>
2023-06-17 21:15:09 +0330

    Add Calculate

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 const show = document.querySelectorAll(".show-display")
 const display = document.querySelector(".display")
+const calculate = document.querySelector(".calculate")
 
 show.forEach(function(event){
     event.addEventListener("click", showDisplay)    
 })
+
+calculate.addEventListener("click",calcu)
+
+function calcu(){
+    display.innerHTML = eval(display.innerHTML)
+}
 
 function showDisplay(){
     if(display.innerHTML == 0){
