6d979f37d7d6d9f9e408101ea0c0195f532280ef
Nima Abed <abodnima84@gmail.com>
2023-06-17 21:09:44 +0330

    Add Event For Show Display

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 const show = document.querySelectorAll(".show-display")
+
+show.forEach(function(event){
+    event.addEventListener("click", showDisplay)    
+})
+
+function showDisplay(){
+    
+}
