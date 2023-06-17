fe101c5f40a9de0a4cfed69aaa8e6d0a8cec20bd
Nima Abed <abodnima84@gmail.com>
2023-06-17 21:10:28 +0330

    Get Display

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const show = document.querySelectorAll(".show-display")
-
+const display = document.querySelector(".display")
 show.forEach(function(event){
     event.addEventListener("click", showDisplay)    
 })
