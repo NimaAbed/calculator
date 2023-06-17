36acbe20ea943c9407b4036a9894202aa8c58a8e
Nima Abed <abodnima84@gmail.com>
2023-06-17 21:12:48 +0330

    Show Number On Display

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const show = document.querySelectorAll(".show-display")
 const display = document.querySelector(".display")
+
 show.forEach(function(event){
     event.addEventListener("click", showDisplay)    
 })
 
 function showDisplay(){
-    
+    if(display.innerHTML == 0){
+        return display.innerHTML = this.innerHTML
+    }    
+    return display.innerHTML += this.innerHTML
 }
